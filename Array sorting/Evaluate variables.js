f3e9d3313ea7c60c3bb39955bb35f03e5c664551
Shashwat365 <skshasha121312@gmail.com>
2025-03-19 21:47:25 +0530

    array

---
+++ Array sorting/Evaluate variables.js
@@ -0,0 +1,11 @@
+function myFunction(){
+    var x=54;
+    var y=21;
+    var a=eval("x + y");
+    var b=eval("2 * 2");
+    var c=eval("x - y");
+   var res=a + b + c;
+   console.log(res);
+}
+myFunction()
+
---
+++ Array sorting/Map an array.js
@@ -0,0 +1,6 @@
+var number =[546 , 45 , 566 , 45546]
+function myFunction(num){
+    return num *10
+}
+var newarray =number.map(myFunction)
+console.log(newarray)
